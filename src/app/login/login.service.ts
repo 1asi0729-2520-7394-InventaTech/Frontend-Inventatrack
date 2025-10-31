d6556d32c75e68aad1b45932b6e5056b2896ea92
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-31 00:26:18 -0500

    Update login.service.ts

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from '../profile/user.model';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
-  private apiUrl = 'https://fake-inventory-api-pb0q.onrender.com/users';
+  private apiUrl = 'https://inventatrack-azekbja3h9eyb0fy.canadacentral-01.azurewebsites.net/api/v1/auth/login'
+
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
-  login(username: string, password: string): Observable<User | null> {
-    return this.http
-      .get<User[]>(`${this.apiUrl}?username=${username}&password=${password}`)
-      .pipe(
-        map(users => (users.length > 0 ? users[0] : null))
-      );
+  login(username: string, password: string): Observable<User> {
+    return this.http.post<User>(this.apiUrl, { username, password }).pipe(
+      tap(user => this.currentUserSubject.next(user))
+    );
+  }
+
+  getCurrentUser(): User | null {
+    return this.currentUserSubject.value;
+  }
+
+  logout(): void {
+    this.currentUserSubject.next(null);
   }
 }
