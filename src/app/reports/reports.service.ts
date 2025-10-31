379e6471787b1068c3aed3ec163391f92cfc823c
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-31 00:30:14 -0500

    Update reports.service.ts

--- src/app/reports/reports.service.ts
+++ src/app/reports/reports.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 export interface Product {
   id: number;
@@ -10,19 +10,25 @@ export interface Product {
   expirationDate: string;
 }
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ReportsService {
-  private apiUrl = 'https://fake-inventory-api-pb0q.onrender.com/inventory';
+  private baseUrl = 'https://inventatrack-azekbja3h9eyb0fy.canadacentral-01.azurewebsites.net/api/reports';
 
   constructor(private http: HttpClient) {}
 
-  getAllProducts(): Observable<Product[]> {
-    return this.http.get<any[]>(this.apiUrl).pipe(
-      map(months => {
-        return months.flatMap(month => month.products);
-      })
-    );
+  getLowStockProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/low-stock`);
+  }
+
+  getExpiringProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/expiring-products`);
+  }
+
+  getExpiringIn14Days(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/expiring-in-14-days`);
+  }
+
+  getExpiredProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/expired-products`);
   }
 }
