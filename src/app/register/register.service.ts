8ed19075803139b3b6f0880bc8c36d6fef74a94b
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-31 00:29:02 -0500

    Create register.service.ts

---
+++ src/app/register/register.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../profile/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class RegisterService {
+  private apiUrl = 'https://inventatrack-azekbja3h9eyb0fy.canadacentral-01.azurewebsites.net/api/v1/users';
+
+  constructor(private http: HttpClient) {}
+
+  register(user: Partial<User>): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+}
