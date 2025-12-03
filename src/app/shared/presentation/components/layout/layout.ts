c286eec51aa32e95b234a3523bcea46d80c5212e
GGonzalo <gonzaloacd@outlook.com>
2025-12-02 21:49:47 -0500

    feat(login): update navbar showing during login and add logout button.

--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 import {LanguageSwitcher} from '../language-switcher/language-switcher';
 import {FooterContent} from '../footer-content/footer-content';
 import {MatButton} from '@angular/material/button';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {TranslatePipe} from '@ngx-translate/core';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -23,11 +24,34 @@ import {TranslatePipe} from '@ngx-translate/core';
   styleUrls: ['./layout.css']
 })
 export class Layout {
+
+  private router = inject(Router);
+
+  showNavbar = signal(true);
+
+
   options = [
     {link: '/home', label: 'option.home'},
     {link: '/inventory', label: 'option.inventory'},
     {link: '/about', label: 'option.about'},
     {link: '/profile', label: 'option.profile'},
-    {link: '/reports', label: 'option.reports'}
+    {link: '/reports', label: 'option.reports'},
+    {link: '/login', label: 'option.login'}
   ]
+
+  constructor() {
+    // Escuchar cambios de navegación
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.checkCurrentRoute();
+    });
+
+    this.checkCurrentRoute();
+  }
+
+  private checkCurrentRoute() {
+    const isLogin = this.router.url.includes('/login');
+    this.showNavbar.set(!isLogin);
+  }
 }
