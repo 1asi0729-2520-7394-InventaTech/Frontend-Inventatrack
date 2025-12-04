5653f0f5ad886a5c6cf04abc906134648ba08b03
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-12-03 23:14:46 -0500

    Update register.service.ts

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../profile/user.model';
+import { LoginService } from '../login/login.service';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
   private apiUrl = 'https://inventatrack-azekbja3h9eyb0fy.canadacentral-01.azurewebsites.net/api/v1/users';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private loginService: LoginService) {}
 
-  register(user: Partial<User>): Observable<any> {
-    return this.http.post<any>(this.apiUrl, user);
+  register(user: Partial<User>): Observable<User> {
+    const token = this.loginService.getToken();
+    if (!token) {
+      throw new Error('Usuario no autorizado. Debe iniciar sesión como admin.');
+    }
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<User>(this.apiUrl, user, { headers });
   }
 }
