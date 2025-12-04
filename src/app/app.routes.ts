7bc1ad7f1a551b16c6726f6c85308cdfaaa32678
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-12-03 22:26:07 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './shared/presentation/views/home/home';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './guards/auth.guard'; 
 
 const about = () =>
     import('./shared/presentation/views/about/about').then(m => m.About);
@@ -17,13 +18,13 @@ const reports = () =>
 const baseTitle = 'InventaTrack';
 
 export const routes: Routes = [
-    { path: 'home', component: Home, title: `${baseTitle} - Home` },
+    { path: 'home', component: Home, title: `${baseTitle} - Home`, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent, title: `${baseTitle} - Login` },
     { path: 'register', component: RegisterComponent, title: `${baseTitle} - Register` },
     { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
-    { path: 'inventory', loadComponent: inventory, title: `${baseTitle} - Inventory` },
-    { path: 'profile', loadComponent: profile, title: `${baseTitle} - Profile` },
-    { path: 'reports', loadComponent: reports, title: `${baseTitle} - Reports` },
+    { path: 'inventory', loadComponent: inventory, title: `${baseTitle} - Inventory`, canActivate: [AuthGuard] },
+    { path: 'profile', loadComponent: profile, title: `${baseTitle} - Profile`, canActivate: [AuthGuard] },
+    { path: 'reports', loadComponent: reports, title: `${baseTitle} - Reports`, canActivate: [AuthGuard] },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not found` },
 ];
