dab4bfe18a3cbcfd42a5fe340e8241ec4d6d31d0
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-12-04 00:41:43 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,21 @@
-import { Routes } from '@angular/router';
 import { Home } from './shared/presentation/views/home/home';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './guards/auth.guard'; 
+
 
 const about = () =>
     import('./shared/presentation/views/about/about').then(m => m.About);
-const pageNotFound = () =>
-    import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
-const inventory = () =>
-    import('./inventory/inventory.component').then(m => m.InventoryComponent);
-const profile = () =>
-    import('./profile/profile.component').then(m => m.ProfileComponent);
-const reports = () =>
-    import('./reports/reports.component').then(m => m.ReportsComponent);
-
+@@ -17,13 +18,13 @@ const reports = () =>
 const baseTitle = 'InventaTrack';
 
 export const routes: Routes = [
-    { path: 'home', component: Home, title: `${baseTitle} - Home`, canActivate: [AuthGuard] },
+    { path: 'home', component: Home, title: `${baseTitle} - Home` },
     { path: 'login', component: LoginComponent, title: `${baseTitle} - Login` },
     { path: 'register', component: RegisterComponent, title: `${baseTitle} - Register` },
     { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
-    { path: 'inventory', loadComponent: inventory, title: `${baseTitle} - Inventory`, canActivate: [AuthGuard] },
-    { path: 'profile', loadComponent: profile, title: `${baseTitle} - Profile`, canActivate: [AuthGuard] },
-    { path: 'reports', loadComponent: reports, title: `${baseTitle} - Reports`, canActivate: [AuthGuard] },
+    { path: 'inventory', loadComponent: inventory, title: `${baseTitle} - Inventory` },
+    { path: 'profile', loadComponent: profile, title: `${baseTitle} - Profile` },
+    { path: 'reports', loadComponent: reports, title: `${baseTitle} - Reports` },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not found` },
 ];
