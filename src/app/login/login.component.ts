35454a103eb4975df05fa13a011ecbcbdd0a4e2d
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-12-03 22:47:22 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { LoginService } from './login.service';
-import { User } from '../profile/user.model';
 
 @Component({
   selector: 'app-login',
@@ -21,26 +20,25 @@ export class LoginComponent {
 
   constructor(private loginService: LoginService, private router: Router) {}
 
-onLogin() {
-  this.loading = true;
-  this.errorMessage = '';
+  onLogin() {
+    this.loading = true;
+    this.errorMessage = '';
 
-  this.loginService.login(this.username, this.password).subscribe({
-    next: (token: string) => {
-      this.loading = false;
-      if (token) {
-        this.router.navigate(['/home']); // redirigir al home
-      } else {
-        this.errorMessage = '❌ Usuario o contraseña inválidos';
+    this.loginService.login(this.username, this.password).subscribe({
+      next: (token: string) => {
+        this.loading = false;
+        if (token) {
+          this.router.navigate(['/home']);
+        } else {
+          this.errorMessage = '❌ Usuario o contraseña inválidos';
+        }
+      },
+      error: () => {
+        this.loading = false;
+        this.errorMessage = '⚠️ Error de conexión con el servidor';
       }
-    },
-    error: () => {
-      this.loading = false;
-      this.errorMessage = '⚠️ Error de conexión con el servidor';
-    }
-  });
-}
-
+    });
+  }
 }
 
 
