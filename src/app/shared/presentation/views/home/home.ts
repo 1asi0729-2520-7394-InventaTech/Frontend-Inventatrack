5f4d4123601e0ca001806ba9d613c52deac4f6b2
Sltcrd <rodrigo.74.atn@gmail.com>
2025-12-05 03:05:49 -0500

    feat(home): update home

--- src/app/shared/presentation/views/home/home.ts
+++ src/app/shared/presentation/views/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  styleUrls: ['./home.css'],
+  standalone: true,
+  imports: [TranslateModule],
 })
 export class Home {}
